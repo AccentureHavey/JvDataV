130e0a7e624ea2292ec022c38948ffdaf0daa685
lrl <15800908003@163.com>
2019-08-07 14:51:11 +0800

    aaa

---
+++ src/components/foot/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="footer-link">
+    <p>Copyright © 2019 Design and Development By Jvisoft</p>
+    <p>
+      Contact Me by Emile :
+      <a
+        href="https:www.Jvisoft.com"
+        target="_blank"
+        title="HongqingCao"
+        >Jvisoft</a
+      >
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "foot"
+};
+</script>
+
+<style lang="scss">
+.footer-link {
+  position: relative;
+  font-size: 18px;
+  bottom: 0;
+  left: 0;
+  color: white;
+  text-align: center;
+  width: 100%;
+  z-index: 1001;
+  p {
+    a {
+      color: #e6e6fa;
+      cursor: pointer;
+    }
+  }
+}
+</style>
